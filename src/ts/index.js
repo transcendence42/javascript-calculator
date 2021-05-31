0d93b8d76683414936edfd0d27bc2c678bd836f5
Yeji Choi <yechoi@student.42seoul.kr>
2021-05-31 21:09:24 +0900

    feat: 사칙연산 기능

--- src/ts/index.js
+++ src/ts/index.js
@@ -2,16 +2,44 @@ function showClickedButton(str) {
     var prompt = document.getElementById('total').innerHTML;
     console.log(str);
     if (prompt === "0") {
-        document.getElementById('total').innerHTML = str;
+        showResult(str);
     }
     else {
-        document.getElementById('total').innerHTML = prompt + str;
+        showResult(prompt + str);
+    }
+}
+function findOperator(str) {
+    if (str.indexOf('/') != -1) {
+        return ('/');
+    }
+    else if (str.indexOf('X') != -1) {
+        return ('X');
+    }
+    else if (str.indexOf('-') != -1) {
+        return ('-');
+    }
+    else if (str.indexOf('+') != -1) {
+        return ('+');
+    }
+    else {
+        return ('none');
     }
 }
 function parseInput(str) {
+    var prompt = document.getElementById('total').innerHTML;
+    var operator = findOperator(prompt);
     var num1;
     var num2;
-    var operator;
+    if (operator !== 'none') {
+        var nums = prompt.split(operator);
+        num1 = nums[0];
+        num2 = nums[1];
+    }
+    else {
+        num1 = prompt;
+        num2 = '1';
+        operator = 'X';
+    }
     return {
         num1: num1,
         num2: num2,
@@ -19,37 +47,58 @@ function parseInput(str) {
     };
 }
 function calculate(num1, num2, operator) {
-    var ret;
+    var ret = '';
+    if (operator === '/') {
+        ret = String(num1 / num2);
+    }
+    else if (operator === 'X') {
+        ret = String(num1 * num2);
+    }
+    else if (operator === '-') {
+        ret = String(num1 - num2);
+    }
+    else if (operator === '+') {
+        ret = String(num1 + num2);
+    }
     return ret;
 }
 function showResult(str) {
+    document.getElementById('total').innerHTML = str;
 }
 function clearPrompt() {
     document.getElementById('total').innerHTML = '';
 }
-function setEventListner() {
+function setDigitsController() {
     var digits = document.getElementsByClassName('digits');
     digits[0].addEventListener('click', function (e) {
         console.log("haha");
         showClickedButton(e.target.innerHTML);
     });
+}
+function setOperationsController() {
     var operations = document.getElementsByClassName('operations');
     operations[0].addEventListener('click', function (e) {
         if (e.target.innerHTML === "=") {
             var input = void 0;
             input = parseInput(document.getElementById('total').innerHTML);
-            showResult(calculate(input.num1, input.num2, input.operator));
-            clearPrompt();
+            showResult(calculate(+input.num1, +input.num2, input.operator));
         }
         else {
             showClickedButton(e.target.innerHTML);
         }
     });
+}
+function setModifierController() {
     var modifier = document.getElementsByClassName('modifier');
     modifier[0].addEventListener('click', function (e) {
         clearPrompt();
         showClickedButton("0");
     });
+}
+function setEventListner() {
+    setDigitsController();
+    setOperationsController();
+    setModifierController();
 }
 function init() {
     setEventListner();
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,66 +1,109 @@
-
 function showClickedButton(str: string) {
-    let prompt : string = document.getElementById('total').innerHTML;
-    console.log(str);
-    if (prompt === "0"){
-        document.getElementById('total').innerHTML = str;
-    } else {
-        document.getElementById('total').innerHTML = prompt + str;
-    }
+  let prompt: string = document.getElementById("total").innerHTML;
+  console.log(str);
+  if (prompt === "0") {
+    showResult(str);
+  } else {
+    showResult(prompt + str);
+  }
 }
 
-function parseInput(str: string) : {num1: number; num2: number; operator: string;} {
-    let num1: number;
-    let num2: number;
-    let operator: string;
-    return {
-        num1: num1,
-        num2: num2,
-        operator: operator
-    }
+function findOperator(str: string): string {
+  if (str.indexOf("/") != -1) {
+    return "/";
+  } else if (str.indexOf("X") != -1) {
+    return "X";
+  } else if (str.indexOf("-") != -1) {
+    return "-";
+  } else if (str.indexOf("+") != -1) {
+    return "+";
+  } else {
+    return "none";
+  }
+}
+
+function parseInput(
+  str: string
+): { num1: string; num2: string; operator: string } {
+  let prompt: string = document.getElementById("total").innerHTML;
+  let operator: string = findOperator(prompt);
+  let num1: string;
+  let num2: string;
+
+  if (operator !== "none") {
+    let nums = prompt.split(operator);
+    num1 = nums[0];
+    num2 = nums[1];
+  } else {
+    num1 = prompt;
+    num2 = "1";
+    operator = "X";
+  }
+  return {
+    num1: num1,
+    num2: num2,
+    operator: operator
+  };
 }
 
 function calculate(num1: number, num2: number, operator: string): string {
-    let ret: string;
-    return ret;
+  let ret: string = "";
+  if (operator === "/") {
+    ret = String(num1 / num2);
+  } else if (operator === "X") {
+    ret = String(num1 * num2);
+  } else if (operator === "-") {
+    ret = String(num1 - num2);
+  } else if (operator === "+") {
+    ret = String(num1 + num2);
+  }
+  return ret;
 }
 
 function showResult(str: string) {
-
+  document.getElementById("total").innerHTML = str;
 }
 
 function clearPrompt() {
-    document.getElementById('total').innerHTML = '';
+  document.getElementById("total").innerHTML = "";
+}
+
+function setDigitsController() {
+  const digits = document.getElementsByClassName("digits");
+  digits[0].addEventListener("click", e => {
+    showClickedButton((<Element>e.target).innerHTML);
+  });
+}
+
+function setOperationsController() {
+  const operations = document.getElementsByClassName("operations");
+  operations[0].addEventListener("click", e => {
+    if ((<Element>e.target).innerHTML === "=") {
+      let input;
+      input = parseInput(document.getElementById("total").innerHTML);
+      showResult(calculate(+input.num1, +input.num2, input.operator));
+    } else {
+      showClickedButton((<Element>e.target).innerHTML);
+    }
+  });
+}
+
+function setModifierController() {
+  const modifier = document.getElementsByClassName("modifier");
+  modifier[0].addEventListener("click", e => {
+    clearPrompt();
+    showClickedButton("0");
+  });
 }
 
 function setEventListner() {
-    const digits = document.getElementsByClassName('digits');
-    digits[0].addEventListener('click', (e) => {
-        console.log("haha");
-        showClickedButton((<Element>e.target).innerHTML);
-    });
-
-    const operations = document.getElementsByClassName('operations');
-    operations[0].addEventListener('click', (e) => {
-        if ((<Element>e.target).innerHTML === "=") {
-            let input; 
-            input = parseInput(document.getElementById('total').innerHTML);
-            showResult(calculate(input.num1, input.num2, input.operator));
-            clearPrompt();        
-        } else {
-            showClickedButton((<Element>e.target).innerHTML);
-        }
-    });
-
-    const modifier = document.getElementsByClassName('modifier');
-    modifier[0].addEventListener('click', (e) => {
-        clearPrompt();
-        showClickedButton("0");
-    });
+  setDigitsController();
+  setOperationsController();
+  setModifierController();
 }
 
 function init() {
-    setEventListner();
+  setEventListner();
 }
 
 init();
