5269a22bb32cd31903b3985869256620da599e41
hochan Lee <hochan049@gmail.com>
2021-06-01 18:47:23 +0900

    test: make case: Max input length 3

--- cypress/integration/counter.spec.js
+++ cypress/integration/counter.spec.js
@@ -1,111 +1,156 @@
-describe("initial value", () => {
+describe('initial value', () => {
   beforeEach(() => {
-    cy.visit("/javascript-calculator/");
+    cy.visit('/javascript-calculator/');
   });
 
-  it("total value", () => {
-    cy.get("#total").should("have.text", "0");
+  it('total value', () => {
+    cy.get('#total').should('have.text', '0');
   });
 });
 
-describe("Render digit when button clicked", () => {
+describe('Render digit when button clicked', () => {
   beforeEach(() => {
-    cy.visit("/javascript-calculator/");
+    cy.visit('/javascript-calculator/');
   });
-  it("button 0", () => {
-    cy.get(".digit")
+  it('button 0', () => {
+    cy.get('.digit')
       .contains(0)
       .click();
-    cy.get("#total").should("have.text", "0");
+    cy.get('#total').should('have.text', '0');
   });
-  it("button 1", () => {
-    cy.get(".digit")
+  it('button 1', () => {
+    cy.get('.digit')
       .contains(1)
       .click();
-    cy.get("#total").should("have.text", "1");
+    cy.get('#total').should('have.text', '1');
   });
-  it("button 2", () => {
-    cy.get(".digit")
+  it('button 2', () => {
+    cy.get('.digit')
       .contains(2)
       .click();
-    cy.get("#total").should("have.text", "2");
+    cy.get('#total').should('have.text', '2');
   });
-  it("button 3", () => {
-    cy.get(".digit")
+  it('button 3', () => {
+    cy.get('.digit')
       .contains(3)
       .click();
-    cy.get("#total").should("have.text", "3");
+    cy.get('#total').should('have.text', '3');
   });
-  it("button 4", () => {
-    cy.get(".digit")
+  it('button 4', () => {
+    cy.get('.digit')
       .contains(4)
       .click();
-    cy.get("#total").should("have.text", "4");
+    cy.get('#total').should('have.text', '4');
   });
-  it("button 5", () => {
-    cy.get(".digit")
+  it('button 5', () => {
+    cy.get('.digit')
       .contains(5)
       .click();
-    cy.get("#total").should("have.text", "5");
+    cy.get('#total').should('have.text', '5');
   });
-  it("button 6", () => {
-    cy.get(".digit")
+  it('button 6', () => {
+    cy.get('.digit')
       .contains(6)
       .click();
-    cy.get("#total").should("have.text", "6");
+    cy.get('#total').should('have.text', '6');
   });
-  it("button 7", () => {
-    cy.get(".digit")
+  it('button 7', () => {
+    cy.get('.digit')
       .contains(7)
       .click();
-    cy.get("#total").should("have.text", "7");
+    cy.get('#total').should('have.text', '7');
   });
-  it("button 8", () => {
-    cy.get(".digit")
+  it('button 8', () => {
+    cy.get('.digit')
       .contains(8)
       .click();
-    cy.get("#total").should("have.text", "8");
+    cy.get('#total').should('have.text', '8');
   });
-  it("button 9", () => {
-    cy.get(".digit")
+  it('button 9', () => {
+    cy.get('.digit')
       .contains(9)
       .click();
-    cy.get("#total").should("have.text", "9");
+    cy.get('#total').should('have.text', '9');
   });
 });
 
-describe("Render digit when calculate +", () => {
+describe('Max input length 3', () => {
   beforeEach(() => {
-    cy.visit("/javascript-calculator/");
+    cy.visit('/javascript-calculator/');
   });
-  it("button 0", () => {
-    cy.get(".digit")
+  it('button 1 2', () => {
+    cy.get('.digit')
       .contains(1)
       .click();
-    cy.get(".operation")
-      .contains("+")
-      .click();
-    cy.get(".digit")
+    cy.get('.digit')
       .contains(2)
       .click();
-    cy.get(".operation")
-      .contains("=")
+    cy.get('#total').should('have.text', '12');
+  });
+  it('button 4 2', () => {
+    cy.get('.digit')
+      .contains(4)
       .click();
-    cy.get("#total").should("have.text", "2");
+    cy.get('.digit')
+      .contains(2)
+      .click();
+    cy.get('#total').should('have.text', '42');
+  });
+  it('button 0 2', () => {
+    cy.get('.digit')
+      .contains(0)
+      .click();
+    cy.get('.digit')
+      .contains(2)
+      .click();
+    cy.get('#total').should('have.text', '2');
+  });
+  it('button 1 2 3', () => {
+    cy.get('.digit')
+      .contains(1)
+      .click();
+    cy.get('.digit')
+      .contains(2)
+      .click();
+    cy.get('.digit')
+      .contains(3)
+      .click();
+    cy.get('#total').should('have.text', '123');
+  });
+  it('button 1 2 3 4', () => {
+    cy.get('.digit')
+      .contains(1)
+      .click();
+    cy.get('.digit')
+      .contains(2)
+      .click();
+    cy.get('.digit')
+      .contains(3)
+      .click();
+    cy.get('.digit')
+      .contains(4)
+      .click();
+    cy.get('#total').should('have.text', '123');
   });
 });
 
-// cy.visit('/javascript-calculator/');
-// let num1 = 0;
-// let num2 = 1;
-
-// while (num2 < 6) {
-//     cy.get('.digit').contains(num1).click();
-//     cy.get('.operation').contains('+').click();
-//     cy.get('.digit').contains(num2).click();
-//     cy.get('.operation').contains('=').click();
-//     cy.get('#total')
-
-//     num1 += 1;
-//     num2 += 1;
-// }
+// describe("Render digit when calculate +", () => {
+//   beforeEach(() => {
+//     cy.visit("/javascript-calculator/");
+//   });
+//   it("button 0", () => {
+//     cy.get(".digit")
+//       .contains(1)
+//       .click();
+//     cy.get(".operation")
+//       .contains("+")
+//       .click();
+//     cy.get(".digit")
+//       .contains(2)
+//       .click();
+//     cy.get(".operation")
+//       .contains("=")
+//       .click();
+//     cy.get("#total").should("have.text", "2");
+//   });
+// });
