e0c8df60608f3e5cecd93875bd467843bc22ca70
hochan Lee <hochan049@gmail.com>
2021-06-01 19:43:44 +0900

    feat: add Calculate two input when button click

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,21 @@
+const calculateOperator = (total, operator, num) => {
+    let result = 0;
+    if (operator === '+')
+        result = total + num;
+    if (operator === '/')
+        result = total / num;
+    if (operator === '%')
+        result = total % num;
+    if (operator === '-')
+        result = total - num;
+    if (operator === '*')
+        result = total * num;
+    return result;
+};
+const calculateResult = (total) => {
+    const result = total.match(new RegExp('(\\??[\\d])(X|\\-|\\+|\\/|\\=)([\\d])'));
+    return calculateOperator(Number(result[1]), result[2], Number(result[3]));
+};
 const digitClickEvent = (e) => {
     const eventTarget = e.target;
     const totalTarget = document.getElementById('total');
@@ -15,6 +33,9 @@ const operatorEvent = (e) => {
     const eventTarget = e.target;
     const totalTarget = document.getElementById('total');
     if (totalTarget.innerText.length === 3) {
+        if (eventTarget.innerText === '=') {
+            document.getElementById('total').innerText = String(calculateResult(document.getElementById('total').innerText));
+        }
         return;
     }
     if (Number(totalTarget.innerText) === 0) {
@@ -28,7 +49,9 @@ export default function App() {
     document.getElementsByClassName('digits')[0].addEventListener('click', e => {
         digitClickEvent(e);
     });
-    document.getElementsByClassName('operations')[0].addEventListener('click', e => {
+    document
+        .getElementsByClassName('operations')[0]
+        .addEventListener('click', e => {
         operatorEvent(e);
     });
 }
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,3 +1,25 @@
+const calculateOperator = (
+  total: number,
+  operator: string,
+  num: number
+): number => {
+  let result: number = 0;
+
+  if (operator === '+') result = total + num;
+  if (operator === '/') result = total / num;
+  if (operator === '%') result = total % num;
+  if (operator === '-') result = total - num;
+  if (operator === '*') result = total * num;
+  return result;
+};
+
+const calculateResult = (total: string): number => {
+  const result: any = total.match(
+    new RegExp('(\\??[\\d])(X|\\-|\\+|\\/|\\=)([\\d])')
+  );
+  return calculateOperator(Number(result[1]), result[2], Number(result[3]));
+};
+
 const digitClickEvent = (e: Event): void => {
   const eventTarget: HTMLElement = e.target as HTMLElement;
   const totalTarget: HTMLElement | null = document.getElementById('total');
@@ -18,11 +40,16 @@ const operatorEvent = (e: Event): void => {
   const totalTarget: HTMLElement | null = document.getElementById('total');
 
   if (totalTarget!.innerText.length === 3) {
+    if (eventTarget.innerText === '=') {
+      document.getElementById('total')!.innerText = String(
+        calculateResult(document.getElementById('total')!.innerText)
+      );
+    }
     return;
   }
 
   if (Number(totalTarget!.innerText) === 0) {
-    return ;
+    return;
   } else {
     document.getElementById('total')!.innerText += eventTarget.innerText;
   }
@@ -32,9 +59,11 @@ export default function App(): void {
   document.getElementsByClassName('digits')[0].addEventListener('click', e => {
     digitClickEvent(e);
   });
-  document.getElementsByClassName('operations')[0].addEventListener('click', e => {
-    operatorEvent(e);
-  });
+  document
+    .getElementsByClassName('operations')[0]
+    .addEventListener('click', e => {
+      operatorEvent(e);
+    });
 }
 
 App();
