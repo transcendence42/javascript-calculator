7f72ab52e471d33a0cf8d9e3b589eb62c68c2985
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2021-06-03 12:13:04 +0900

    feat: add click num

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,5 +3,24 @@ import Equation from "./equation.js";
 export default class App {
   constructor() {
     const equation = new Equation();
+    const totalDiv = document.querySelector("#total") as HTMLDivElement;
+    //click num
+    document
+      .querySelector("div.digits.flex")!
+      .addEventListener("click", evt => {
+        if ((evt.target as HTMLElement).className !== "digit") {
+          return;
+        }
+        if (isNaN(Number(totalDiv.innerText))) {
+          totalDiv.innerText = "0";
+        }
+        if (totalDiv!.innerText.length < 3) {
+          totalDiv!.insertAdjacentText(
+            "beforeend",
+            (evt.target as HTMLButtonElement).innerText
+          );
+          totalDiv.innerText = String(Number(totalDiv.innerText));
+        }
+      });
   }
 }
